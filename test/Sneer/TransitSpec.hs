c8ff2e7823b99feac8134062f6816469d2382f86
Rodrigo B. de Oliveira <rbo@acm.org>
2015-04-01 23:57:20 -0300

    enable hspec-discover, introduce Sneer.TransitSpec

---
+++ test/Sneer/TransitSpec.hs
@@ -0,0 +1,10 @@
+module Sneer.TransitSpec where
+
+import Sneer.Transit
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "tson" $
+    it "can encode strings" $
+      tson "foo" `shouldBe` jstring "bar"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,1 @@
-module Main where
-
-import Sneer.Transit
-import Test.Hspec
-
-main :: IO ()
-main = hspec $
-  describe "Sneer.Transit" $
-    it "can encode strings" $
-      tson "foo" `shouldBe` jstring "bar"
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
