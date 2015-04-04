4b743d47ec9ad4a79fe735381d691d2025808921
Rodrigo B. de Oliveira <rbo@acm.org>
2015-04-04 20:34:16 -0300

    [travis-ci] longer timeout for tests when running on CI server

--- test/Sneer/ClientSpec.hs
+++ test/Sneer/ClientSpec.hs
@@ -3,6 +3,7 @@ module Sneer.ClientSpec where
 import Control.Concurrent.Async
 import Sneer.Client
 import Sneer.Transit
+import System.Environment (lookupEnv)
 import System.Timeout
 import Test.Hspec
 
@@ -16,11 +17,23 @@ spec =
                            ,(k "from", k "me")
                            ,(k "to",   k "you")
                            ]
-      Just _ <- timeoutIn 5 seconds $ wait pendingAck
+      Just _ <- withTimeout $ wait pendingAck
       return ()
  where
   tuple = Map [("id", Number 42)]
   k = Keyword
+
+withTimeout :: IO a -> IO (Maybe a)
+withTimeout action = do
+  t <- defaultTimeout
+  timeoutIn t seconds action
+
+defaultTimeout :: IO Int
+defaultTimeout = do
+  ci <- lookupEnv "CI"
+  return $ case ci of
+    Just _  -> 30
+    Nothing -> 1
 
 timeoutIn :: Int -> (Int -> Int) -> IO a -> IO (Maybe a)
 timeoutIn t unit = timeout (unit t)
